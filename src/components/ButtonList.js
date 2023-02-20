718d83d7e71ab6f4a059bff210b73085069013f2
deepak mittal <deepakm905@gmail.com>
2023-02-20 19:17:56 +0530

    first checkin for youtube code

---
+++ src/components/ButtonList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "./Button";
+
+const ButtonList = () => {
+  return (
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Gaming" />
+      <Button name="Songs" />
+      <Button name="Soccer" />
+      <Button name="Cricket" />
+      <Button name="Cooking" />
+      <Button name="Valentine" />
+      <Button name="Gaming" />
+      <Button name="Gaming" />
+    </div>
+  );
+};
+
+export default ButtonList;
---
+++ src/components/VideoContainer.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../Utils/Constant";
+import VideoCard from "./VideoCard";
+
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const videosjson = await data.json();
+    setVideos(videosjson.items);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
